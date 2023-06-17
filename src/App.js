5758f39808e910ef654c68314490702a14e52764
kanamydamiao <kanamystewart@gmail.com>
2023-06-17 10:34:16 -0300

    página de perfil de usuário

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Donations from "./pages/donations/Donations";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Terms from "./pages/terms/Terms";
-import MyPerfil from "./pages/myPerfil/MyPerfil";
+import MyProfile from "./pages/myProfile/MyProfile";
 import MyDonations from "./pages/myDonations/MyDonations";
 import NewDonation from "./pages/newDonation/NewDonation";
 import EditDonation from "./pages/editDonation/EditDonation";
@@ -20,7 +20,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/politicas-de-uso" element={<Terms />} />
           <Route path="/cadastro" element={<Register />} />
-          <Route path="/meu-perfil" element={<MyPerfil />} />
+          <Route path="/meu-perfil" element={<MyProfile />} />
           <Route path="/minhas-doacoes" element={<MyDonations />} />
           <Route path="/nova-doacao" element={<NewDonation />} />
           <Route path="/editar-doacao/:id" element={<EditDonation />} />
